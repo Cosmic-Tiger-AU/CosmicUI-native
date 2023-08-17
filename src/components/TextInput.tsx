404c801f82a464abb0f0adfc4480620643fa769d
Matt Dwyer <mattsd1410@gmail.com>
2023-08-17 15:21:17 +1000

    Handle the extra colour props on TextInput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,6 +1,32 @@
+import React from "react";
 import { TextInput as BaseTextInput, TextInputProps } from "react-native";
-import withTextStyle from "~/util/withTextStyle";
+import { useTheme } from "./CosmicProvider";
+import { ColorTypes, getColor } from "~/types/color-types";
+import { CosmicTextStyle } from "~/types/cosmic-text-style";
+import withTextStyle, { CosmicTextProps } from "~/util/withTextStyle";
 
-const TextInput = withTextStyle<TextInputProps>(BaseTextInput);
+const StyledTextInput = withTextStyle<TextInputProps>(BaseTextInput);
+
+export type CosmicTextInputProps = CosmicTextProps<TextInputProps> & {
+  style?: CosmicTextStyle;
+  cursorColor?: ColorTypes,
+  placeholderTextColor?: ColorTypes,
+  selectionColor?: ColorTypes,
+} & React.RefAttributes<any>;
+
+const TextInput = React.forwardRef<
+  React.ElementRef<typeof StyledTextInput>,
+  TextInputProps & CosmicTextInputProps
+>(({ ...props }, ref) => {
+  const theme = useTheme();
+
+  const cursorColor = getColor(theme, props.cursorColor || "primary");
+  const placeholderTextColor = getColor(theme, props.placeholderTextColor || "lightGray");
+  const selectionColor = getColor(theme, props.selectionColor || "primary");
+
+  const colorProps = { cursorColor, placeholderTextColor, selectionColor };
+
+  return <StyledTextInput ref={ref} {...props} {...colorProps} />;
+});
 
 export default TextInput;
